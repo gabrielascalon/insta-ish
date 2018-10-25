59aed912d7c48aab4dddef3835743f4dd81d0cf3
gabrielascalon <gbscalon@gmail.com>
2018-10-24 23:31:57 -0300

    Initial commit

--- api/photo_app/models.py
+++ api/photo_app/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     # user = models.ForeignKey('auth.User', models.on_delete=CASCADE)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='static/photos/')
     description = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 
