4ad3b6fffb40e480d949a371ab0b3d988e29c1f8
gabrielascalon <gbscalon@gmail.com>
2018-11-03 20:30:13 -0300

    uptaded the code and gitignore

--- api/photo_app/models.py
+++ api/photo_app/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     # user = models.ForeignKey('auth.User', models.on_delete=CASCADE)
     image = models.ImageField(upload_to='static/photos/')
-    description = models.TextField()
+    description = models.TextField(blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- api/photo_app/serializers.py
+++ api/photo_app/serializers.py
@@ -6,3 +6,9 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
         fields = ('image', 'description', 'published_date')
+
+    def validate(self, data):
+        print('*' * 80)
+        print(data)
+        print('*' * 80)
+        return data
